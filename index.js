d6a5102864af258190729e4a09379ab75879410f
chagchen <chagchen@outlook.com>
2023-05-23 20:33:52 -0700

    frontend main.js with css

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const dotenv = require('dotenv').config();
 const port = process.env.PORT || 8080;
 //initialize express
 const app = express();
+const path = require('path');
 
 //enable body parser
 app.use(express.json());
 //this allows to accept body data. 
 app.use(express.urlencoded({extended: false}));
+
+//setup static folder added also path module above to do so. 
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/openai', require('./routes/openaiRoutes'));
 
